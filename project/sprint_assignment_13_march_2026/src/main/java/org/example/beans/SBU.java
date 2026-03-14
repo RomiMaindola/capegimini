2969c9b35ac0d4be517927cc2b2be04de82aa997
ROMI MAINDOLA <romimaindola9411@gmail.com>
2026-03-14 13:00:28 +0530

    Update SBU.java

--- project/sprint_assignment_13_march_2026/src/main/java/org/example/beans/SBU.java
+++ project/sprint_assignment_13_march_2026/src/main/java/org/example/beans/SBU.java
@@ -1,13 +1,22 @@
 package org.example.beans;
 
+import java.util.List;
+
 public class SBU {
     private  int subId;
     private String sbuName;
     private String sbuHead;
-
+    private List<Employee> emp;
+    
     public SBU() {
     }
-
+    
+public List<Employee> getEmp() {
+    return emp;
+}
+public void setEmp(List<Employee> emp) {
+    this.emp = emp;
+}
     public SBU(int subId, String sbuName, String sbuHead) {
         this.subId = subId;
         this.sbuName = sbuName;
